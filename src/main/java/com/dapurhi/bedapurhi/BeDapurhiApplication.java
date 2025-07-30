bbac9a0c248aab952b9763e8c548296a899f99b4
Muhammad Hudaibi <muhhudaibi11@gmail.com>
2025-07-31 00:46:35 +0700

    initial commit

---
+++ src/main/java/com/dapurhi/bedapurhi/BeDapurhiApplication.java
@@ -0,0 +1,13 @@
+package com.dapurhi.bedapurhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeDapurhiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeDapurhiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dapurhi/bedapurhi/BeDapurhiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dapurhi.bedapurhi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeDapurhiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
